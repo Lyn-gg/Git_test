667044f6e31bdaa0321f3653e88261dcdf5f1c2b
Lyn-gg <122553484+Lyn-gg@users.noreply.github.com>
2023-01-15 00:00:58 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("Hello,git--hotfix2!");
         System.out.println("push test");
         System.out.println("push test2");
-        System.out.println("pull test");
+        System.out.println("pull test1");
     }
 }
